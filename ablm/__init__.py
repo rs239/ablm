426dd3025ef711ecc5ff333e7d8dead0d94531fb
lordim <chihoim@mit.edu>
2023-02-21 10:17:01 -0500

    changed dir names

--- ablm/__init__.py
+++
@@ -1,3 +0,0 @@
-# Which modules will users want to use most often? Maybe import those
-from ablm import abmap_embed
-from ablm import main # TODO - change name of main to abml or abmap? Would allow (from ablm import abmap)
