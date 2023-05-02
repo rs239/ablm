bf4d9d0f41d49581069db883cc463204932174b6
Taylor.Sorenson <taylor.sorenson@sanofi.com>
2023-05-02 12:37:56 -0700

    version now found in abmap/version.py, remove abmap dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-# import abmap
+
+with open("README.md") as f:
+    readme = f.read()
+    
+with open("abmap/version.py") as file:
+    exec(file.read())
 
 setup(
     name="abmap",
-    version=abmap.__version__,
+    version=version,
     description="AbMAP: Antibody Mutagenesis Augmented Processing",
+    long_description=readme, 
     author="Chiho Im, Taylor Sorenson, Abhinav Gupta, Rohit Singh",
     author_email="chihoim@mit.edu",
     # url="http://dscript.csail.mit.edu",
