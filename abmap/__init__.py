b97fd809245c570f0fcfec002a31f7447f8964fb
Taylor Sorenson <taylor.sorenson@sanofi.com>
2023-05-02 12:20:08 -0700

    Test

--- abmap/__init__.py
+++ abmap/__init__.py
@@ -1,7 +1,7 @@
-# from abmap.abmap_augment import ProteinEmbedding, augment_from_fasta
-# from abmap.plm_embed import reload_models_to_device
-# from abmap.model import AbMAPAttn, AbMAPLSTM
-# from abmap.commands.embed import load_abmap, abmap_embed, abmap_embed_batch
+from abmap.abmap_augment import ProteinEmbedding, augment_from_fasta
+from abmap.plm_embed import reload_models_to_device
+from abmap.model import AbMAPAttn, AbMAPLSTM
+from abmap.commands.embed import load_abmap, abmap_embed, abmap_embed_batch
 
 __version__ = "0.0.88"
 # __citation__ = """"""
