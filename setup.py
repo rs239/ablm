b61809f92ad2a94c54e3df2b43eb083df374997d
Taylor.Sorenson <taylor.sorenson@sanofi.com>
2023-05-02 12:42:07 -0700

    move license according to new requirements

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     author="Chiho Im, Taylor Sorenson, Abhinav Gupta, Rohit Singh",
     author_email="chihoim@mit.edu",
     # url="http://dscript.csail.mit.edu",
-    license="MIT",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
