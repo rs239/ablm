b34f91558e7458d091c629a57f496b0ff0e84901
lordim <chihoim@mit.edu>
2023-05-02 19:50:37 -0400

    fixing merge conflict

--- abmap/__init__.py
+++ abmap/__init__.py
@@ -1,15 +1,12 @@
 # from abmap.abmap_augment import ProteinEmbedding, augment_from_fasta
 # from abmap.plm_embed import reload_models_to_device
-# from abmap.model import AbMAPAttn, AbMAPLSTM
+# from abmap.model import AbMAPAttn
 # from abmap.commands.embed import load_abmap, abmap_embed, abmap_embed_batch
 
-__version__ = "0.0.88"
+__version__ = "0.0.89"
 # __citation__ = """"""
 from . import (
     commands,
-    # abmap_augment,
-    # plm_embed,
-    # model,
 )
 
 __all__ = [
